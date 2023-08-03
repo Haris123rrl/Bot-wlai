7b85f3f78949fef090776da7e867165a82855bdf
Haris123rrl <hzhshsjsjajaja@gmail.com>
2023-08-03 09:12:44 +0700

    bebas

---
+++ config.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.apikey = '3d27cc6592c983dd253f642f' //https://api.lolhuman.xyz
+global.rosekey = '-' //https://api.itsrose.life
+//—————「 Set Nama Bot & Own 」—————//
+global.namabot = 'rara'
+global.namaowner = 'haris'
+
+//—————「 Setting Owner 」—————//
+global.owner = '6285936162097'
+global.ownernomer = ["085852598380"]
+global.premium = ['085852598380']
+
+//—————「 Set Wm 」—————//
+global.packname = ''
+global.author = 'bot'
+global.prefa = ['', '.']
+global.sp = '•'
+
+//—————「 Set Message 」—————//
+global.mess = {
+    done: 'Done ✅',
+    admin: 'Feature Only for _*Admin Group*_',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin Group !',
+    owner: 'Feature Only for _*owner*_',
+    group: 'Feature Only for _*Group Chat*_',
+    private: 'Feature Only for _*Admin Group*_',
+    wait: 'Wait a Moment, for Process',
+    endLimit: 'Your imit has run out, Wait at 12 at night',
+    error: '*!!!Feature Error!!!*',
+}
+
+//—————「 Set Limit 」—————//
+global.limitawal = {
+    premium: "Infinity",
+    free: 50,
+}
+
+//—————「 Set Image 」—————//
+global.imageurl = 'https://telegra.ph/file/a786cdf373da5171cb70d.jpg'
+global.isLink = `https://www.youtube.com/@Haris.m`
+global.thumb = fs.readFileSync('./media/thumb.jpg')
+
+//—————「 Batas Akhir 」—————//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+    fs.unwatchFile(file)
+    console.log(chalk.redBright(`Update'${__filename}'`))
+    delete require.cache[file]
+    require(file)
+})
